8eb9ccc8907e674be277a804a331b469226bd6b1
Shree Rai <shreerai1@outlook.com>
2022-05-02 17:59:34 +0545

    error append fixed

--- yolo_to_pascal_parser.py
+++ yolo_to_pascal_parser.py
@@ -2,57 +2,67 @@ import pandas as pd
 import os
 import xml.etree.ElementTree as eltree
 
+
 df = pd.read_csv('./Train_car_split.csv')
-DIRECTORY_NAME = os.path.basename(os.getcwd())
-FULL_PATH = os.path.abspath(os.getcwd())
-ROOT_ELEM = eltree.Element("Main")
-FOLDER_NAME_ELEM = eltree.Element("folder")
-FULL_PATH_ELEM = eltree.Element("path")
-SOURCE_ELEM = eltree.Element("source")
-SIZE_ELEM = eltree.Element("size")
-SEGMENTED_ELEM = eltree.Element("segmented")
-OBJECT_ELEM = eltree.Element("object")
-BOUNDING_BOX_ELEM = eltree.Element("bndbox")
+directory_name = os.path.basename(os.getcwd())
+full_path = os.path.abspath(os.getcwd())
+root_elem = eltree.Element("main")
+folder_name_elem = eltree.Element("folder")
+full_path_elem = eltree.Element("path")
+source_elem = eltree.Element("source")
+size_elem = eltree.Element("size")
+segmented_elem = eltree.Element("segmented")
+object_elem = eltree.Element("object")
+bounding_box_elem = eltree.Element("bndbox")
+database_elem = eltree.SubElement(source_elem, "database")
+file_name_elem = eltree.Element("filename")
+width_elem = eltree.SubElement(size_elem, "width")
+height_elem = eltree.SubElement(size_elem, "height")
+depth_elem = eltree.SubElement(size_elem, "depth")
+
+name_sub_elem = eltree.SubElement(object_elem, "name")
+pose_sub_elem = eltree.SubElement(object_elem, "pose")
+
+truncated_sub_elem = eltree.SubElement(object_elem, "truncated")
+truncated_sub_elem.text = '0'
+
+difficult_sub_elem = eltree.SubElement(object_elem, "difficult")
+difficult_sub_elem.text = '0'
+
+root_elem.append(folder_name_elem)
+root_elem.append(full_path_elem)
+root_elem.append(source_elem)
+root_elem.append(size_elem)
+root_elem.append(segmented_elem)
+root_elem.append(object_elem)
+root_elem.append(file_name_elem)
+
+bounding_box_elem = eltree.SubElement(object_elem, "bndbox")
+x_min = eltree.SubElement(bounding_box_elem, "xmin")
+y_min = eltree.SubElement(bounding_box_elem, "ymin")
+x_max = eltree.SubElement(bounding_box_elem, "xmax")
+y_max = eltree.SubElement(bounding_box_elem, "ymax")
 
 for index, row in df.iterrows():
+    file_name = str(row['path'])
 
-    file_name = row['path']
+    folder_name_elem.text = directory_name
+    file_name_elem.text = file_name
+    full_path_elem.text = full_path
 
-    ROOT_ELEM.append(FOLDER_NAME_ELEM)
-    ROOT_ELEM.append(FULL_PATH_ELEM)
-    ROOT_ELEM.append(SOURCE_ELEM)
-    ROOT_ELEM.append(SIZE_ELEM)
-    ROOT_ELEM.append(SEGMENTED_ELEM)
-    ROOT_ELEM.append(OBJECT_ELEM)
-    FOLDER_NAME_ELEM.text = DIRECTORY_NAME
-    FILE_NAME_ELEM = eltree.Element("filename")
-    ROOT_ELEM.append(FILE_NAME_ELEM)
-    FILE_NAME_ELEM.text = file_name
-    FULL_PATH_ELEM.text = FULL_PATH
-    DATABASE_ELEM = eltree.SubElement(SOURCE_ELEM, "database")
-    DATABASE_ELEM.text = "Unknown"
-    WIDTH_ELEM = eltree.SubElement(SIZE_ELEM, "width")
-    HEIGHT_ELEM = eltree.SubElement(SIZE_ELEM, "height")
-    DEPTH_ELEM = eltree.SubElement(SIZE_ELEM, "depth")
-    SEGMENTED_ELEM.text = "0"
-    NAME_SUB_ELEM = eltree.SubElement(OBJECT_ELEM, "name")
-    NAME_SUB_ELEM.text = row['class_name']
-    POSE_SUB_ELEM = eltree.SubElement(OBJECT_ELEM, "pose")
-    POSE_SUB_ELEM.text = 'unspecified'
-    TRUNCATED_SUB_ELEM = eltree.SubElement(OBJECT_ELEM, "truncated")
-    TRUNCATED_SUB_ELEM.text = '0'
-    DIFFICULT_SUB_ELEM = eltree.SubElement(OBJECT_ELEM, "difficult")
-    DIFFICULT_SUB_ELEM.text = '0'
-    BOUNDING_BOX_ELEM = eltree.SubElement(OBJECT_ELEM, "bndbox")
-    X_MIN = eltree.SubElement(BOUNDING_BOX_ELEM, "xmin")
-    Y_MIN = eltree.SubElement(BOUNDING_BOX_ELEM, "ymin")
-    X_MAX = eltree.SubElement(BOUNDING_BOX_ELEM, "xmax")
-    Y_MAX = eltree.SubElement(BOUNDING_BOX_ELEM, "ymax")
-    X_MIN.text = str(row['x1'])
-    Y_MIN.text = str(row['y1'])
-    X_MAX.text = str(row['x2'])
-    Y_MAX.text = str(row['y2'])
-    tree = eltree.ElementTree(ROOT_ELEM)
+    database_elem.text = "Unknown"
+    segmented_elem.text = "0"
+
+    name_sub_elem.text = str(row['class_name'])
+
+    pose_sub_elem.text = 'unspecified'
+
+    x_min.text = str(row['x1'])
+    y_min.text = str(row['y1'])
+    x_max.text = str(row['x2'])
+    y_max.text = str(row['y2'])
+    tree = eltree.ElementTree(root_elem)
 
     with open(os.path.splitext(file_name)[0]+".xml", "wb") as files:
         tree.write(files)
+        files.close()
